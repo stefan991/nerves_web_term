bdedd3549f1489ebc935aa4b6c2d967351a75c41
Stefan Hoffmann <stefan991@gmail.com>
2020-09-17 14:16:06 +0200

    fix connection timeout with websockets ping pong

--- lib/nerves_web_term/socket_handler.ex
+++ lib/nerves_web_term/socket_handler.ex
@@ -1,6 +1,8 @@
 defmodule NervesWebTerm.SocketHandler do
   @behaviour :cowboy_websocket
 
+  @ping_time 20000
+
   def init(request, _state) do
     state = %{}
     {:cowboy_websocket, request, state}
@@ -10,12 +12,26 @@ defmodule NervesWebTerm.SocketHandler do
     Registry.NervesWebTerm
     |> Registry.register("TERMINAL", {})
 
+    Process.send_after(self(), :send_ping, @ping_time)
+
     {:ok, state}
   end
 
   def websocket_handle({:text, data}, state) do
     NervesWebTerm.UART.write(data)
     {:ok, state}
+  end
+
+  def websocket_handle(:pong, state) do
+    # do nothing...
+    {:ok, state}
+  end
+
+  def websocket_info(:send_ping, state) do
+    # Send ping messages to keep connection alive.
+    Process.send_after(self(), :send_ping, @ping_time)
+
+    {:reply, :ping, state}
   end
 
   def websocket_info(info, state) do
